854cc61e9c47496bccd3fecfa7e928b1f702f01b
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-04-07 17:01:05 -0300

    NLW Unite | Check-in, documentation and customize errors messages in application

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,41 @@
 import fastify from "fastify";
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+import { errorHandler } from "./error-handler";
+import { CheckIn } from "./routes/check-in";
 import { createEvent } from "./routes/create-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { getEvent } from "./routes/get-event";
+import { getEventAttendees } from "./routes/get-event-attendees";
 import { RegisterForEvent } from "./routes/register-for-event";
 
-const app = fastify()
+import { fastifyCors } from "@fastify/cors";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+	origin: '*',
+})
+
+app.register(fastifySwagger, {
+	swagger: {
+		consumes: ['application/json'],
+		produces: ['application/json'],
+		info: {
+			title: 'pass.in',
+			description: 'Espicificações da API para o back-end da aplicação pass.in construída durante o NLW da Rocketseat',
+			version: '1.0.0'
+		},
+	},
+	transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+	routePrefix: '/docs'
+})
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -14,7 +44,11 @@ app.register(createEvent)
 app.register(RegisterForEvent)
 app.register(getEvent)
 app.register(getAttendeeBadge)
+app.register(CheckIn)
+app.register(getEventAttendees)
 
-app.listen({ port: 3333 }).then(() => {
+app.setErrorHandler(errorHandler)
+
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
 	console.log('HTTP server running!')
 })
